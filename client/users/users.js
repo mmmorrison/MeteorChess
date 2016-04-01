fe51c4a73707ffd7a4ee1c2567f28d5e6c0dacbe
Maria <mmorrison1978@gmail.com>
2016-04-01 13:35:26 -0600

    added event listener

--- client/users/users.js
+++ client/users/users.js
@@ -6,4 +6,14 @@ Template.users.helpers({
   users: function () {
     return Meteor.users.find({ username: { $not: (Meteor.user() || {}).username}})
   }
+});
+
+Template.user.helpers({
+  alreadyFriends: alreadyFriends
+});
+
+Template.user.events({
+  'click .add': function (evt) {
+    Meteor.call('setFriend', this._id)
+  }
 })
