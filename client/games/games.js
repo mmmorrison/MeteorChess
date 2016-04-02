5ad7473c527aac6d35b77313b49550ccf865864c
Maria <mmorrison1978@gmail.com>
2016-04-02 11:34:16 -0600

    added games helper

--- client/games/games.js
+++ client/games/games.js
@@ -1,4 +1,18 @@
 Template.games.onCreated(function() {
   this.subscribe('users');
   this.subscribe('games');
-})
+});
+
+Template.games.helpers(function() {
+  possibleOpponents: function() {
+    var users = Meteor.user();
+    var friends = user.profile.friends || [];
+
+    Games.find({ result: null }).forEach(function (games) {
+      var color = (games.w === user.id) : 'b' : 'w';
+
+      games[color]
+    })
+
+  }
+});
