2c4d485cc5f7bc2e50c4f5cdf4eb7ce7cde48fa8
Maria <mmorrison1978@gmail.com>
2016-04-03 15:19:56 -0600

    added pieces objects

--- client/game/game.js
+++ client/game/game.js
@@ -1,37 +1,37 @@
 Template.game.OnCreated(function() {
-  this.autorun(() => {
-    //autorun() will rerun the function whenever the value of the reactive function is changed.
-    this.subscribe('users');
-    this.subscribe('game', FlowRouter.getParam('id'))
-    //FlowRouter.getParam() is the reactive function that will autorun is the value changes
-  })
+    this.autorun(() => {
+        //autorun() will rerun the function whenever the value of the reactive function is changed.
+        this.subscribe('users');
+        this.subscribe('game', FlowRouter.getParam('id'))
+            //FlowRouter.getParam() is the reactive function that will autorun is the value changes
+    })
 })
 
 Template.game.helpers({
-  currentTurn: function() {
-    var game = getGame();
-    return getUsername(game[game.board.split(' ')[1]])
-  },
+    currentTurn: function() {
+        var game = getGame();
+        return getUsername(game[game.board.split(' ')[1]])
+    },
 
-  result: function(){
-    var result = getGame().result;
+    result: function() {
+        var result = getGame().result;
 
-    if (!result) return null;
-    if (result === 'draw') return 'Draw!';
+        if (!result) return null;
+        if (result === 'draw') return 'Draw!';
 
-    return getUsername(result) + ' won!'
-  },
+        return getUsername(result) + ' won!'
+    },
 
-  moves: function(){
-    return pair(getMoves()).map(function(arr) {
-      return arr[0] + ' ' + (arr[1] || '');
-    });
-  },
+    moves: function() {
+        return pair(getMoves()).map(function(arr) {
+            return arr[0] + ' ' + (arr[1] || '');
+        });
+    },
 
-  row: function(){
-    var chess = new Chess();
-    getMoves().forEach(chess.move.bind(chess));
-    return makeRows(chess.fen(), getGame().b)
-    //fen = Forsyth-Edwards Notation for chess moves
-  }
+    row: function() {
+        var chess = new Chess();
+        getMoves().forEach(chess.move.bind(chess));
+        return makeRows(chess.fen(), getGame().b)
+            //fen = Forsyth-Edwards Notation for chess moves
+    }
 });
