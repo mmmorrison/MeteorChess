cbdd097af66f53e3d579b793e3825459c4d76981
Maria <mmorrison1978@gmail.com>
2016-04-03 14:06:29 -0600

    added getMoves function for replay

--- server/main.js
+++ server/main.js
@@ -6,3 +6,8 @@ Meteor.publish('games', function () {
   this.userId
   return Games.find({ $or: [{b:this.userId}, {w:this.userId}]})
 });
+
+Meteor.publish('game', function (gameId) {
+  // Meteor won't allow you to return a single item in a publication.  You have return an object that could hold multiple items.
+  return Games.find({_id: gameId})
+})
