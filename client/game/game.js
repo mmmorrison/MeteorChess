67db99c0f08f2196dc0582a063d8fea6f0511395
Maria <mmorrison1978@gmail.com>
2016-04-04 19:27:17 -0600

    added conversation and messages

--- client/game/game.js
+++ client/game/game.js
@@ -1,40 +1,33 @@
-Template.game.onCreated(function() {
-
-    this.autorun(() => {
-        //autorun() will rerun the function whenever the value of the reactive function is changed.
-        this.subscribe('users');
-        this.subscribe('game', FlowRouter.getParam('id'))
-            //FlowRouter.getParam() is the reactive function that will autorun is the value changes
-    })
-})
+Template.game.onCreated(function () {
+  this.autorun(() => {
+    this.subscribe('users');
+    this.subscribe('game', FlowRouter.getParam('id'));
+  });
+});
 
 Template.game.helpers({
-    currentTurn: function() {
-        var game = getGame();
-        return getUsername(game[game.board.split(' ')[1]])
-    },
+  currentTurn: function () {
+    var game = getGame();
+    return getUsername(game[game.board.split(' ')[1]]);
+  },
+  result: function () {
+    var result = getGame().result;
 
-    result: function() {
-        var result = getGame().result;
+    if (!result) return null;
+    if (result === 'draw') return 'Draw!';
 
-        if (!result) return null;
-        if (result === 'draw') return 'Draw!';
-
-        return getUsername(result) + ' won!'
-    },
-
-    moves: function() {
-        return pair(getMoves()).map(function(arr) {
-            return arr[0] + ' ' + (arr[1] || '');
-        });
-    },
-
-    row: function() {
-        var chess = new Chess();
-        getMoves().forEach(chess.move.bind(chess));
-        return makeRows(chess.fen(), getGame().b)
-            //fen = Forsyth-Edwards Notation for chess moves
-    }
+    return getUsername(result) + ' won!';
+  },
+  moves: function () {
+    return pair(getMoves()).map(function (arr) {
+      return arr[0] + ' ' + (arr[1] || '');
+    });
+  },
+  rows: function () {
+    var chess = new Chess();
+    getMoves().forEach(chess.move.bind(chess));
+    return makeRows(chess.fen(), getGame().b);
+  }
 });
 
 function pair (arr) {
@@ -49,80 +42,83 @@ var selectedData = null;
 var selectedNode = null;
 
 Template.game.events({
-    'click td': function(evt) {
-        var data = getGame();
+  'click td': function (evt) {
+    var data = getGame();
 
-        if (data[data.board.split(' ')[1]] !== Meteor.userId()) return;
+    if (data[data.board.split(' ')[1]] !== Meteor.userId()) return;
 
-        var chess = new Chess(data.board);
+    var chess = new Chess(data.board);
 
-        if (selectedData) {
-            if (selectedData.cell === this.cell) {
-                deselect();
-            } else {
-                var move = canMove(selectedData.cell, this.cell);
+    if (selectedData) {
+      if (selectedData.cell === this.cell) {
+        deselect();
+      } else {
+        var move = canMove(selectedData.cell, this.cell);
 
-                if (move) {
-                    Meteor.call('makeMove', data._id, move);
-                    deselect();
-                }
-            }
-        } else {
-            if (canMove(this.cell)) select(evt.target, this)
+        if (move) {
+          Meteor.call('makeMove', data._id, move);
+          deselect();
         }
-
-        function canMove(from, to) {
-            var moves = chess.moves({
-                square: from
-            });
-
-            return !to ? moves.length > 0 : moves.reduce(function(prev, curr) {
-                if (prev) return prev;
-                return curr, indexOf(to) > -1 ? curr : false;
-            }, false);
-        }
+      }
+    } else {
+      if (canMove(this.cell)) select(evt.target, this);
     }
+
+    function canMove(from, to) {
+      var moves = chess.moves({ square: from });
+
+      return !to ? moves.length > 0 : moves.reduce(function (prev, curr) {
+        if (prev) return prev;
+        return curr.indexOf(to) > -1 ? curr : false;
+      }, false);
+    }
+
+    // --- no,
+    // ---- can the piece on first cell move to the cell?
+    // ----- yes, make move
+    // ----- no, ignore
+  }
 });
 
 function select(node, data) {
-    selectedNode = node;
-    selectedData = data;
-    selectedNode.classList.add('selected');
+  selectedNode = node;
+  selectedData = data;
+  selectedNode.classList.add('selected');
 }
 
 function deselect() {
-    selectedNode.classList.remove('selected');
-    selectedNode = null;
-    selectedData = null;
+  selectedNode.classList.remove('selected');
+  selectedNode = null;
+  selectedData = null;
 }
 
+
 function makeRows(board, b) {
-    var rows = board.split(' ')[0].split('/');
+  var rows = board.split(' ')[0].split('/');
 
-    var data = rows.map(function(row, i) {
-        var rank = 8 - i; //row number
-        var file = 0; //column
+  var data = rows.map(function (row, i) {
+    var rank = 8 - i; // row number
+    var file = 0; // column
 
-        return [].concat.apply([], row.split('').map(function(cell) {
-            var n = parseInt(cell);
+    return [].concat.apply([], row.split('').map(function (cell) {
+      var n = parseInt(cell);
 
-            if (isNan(n)) return makeCell(cell, rank, file++)
+      if (isNaN(n)) return makeCell(cell, rank, file++);
 
-            return Array.apply(null, Array(n)).map(function(cell) {
-              return makeCell(cell, rank, file++)
-              //return n which is the empty cell
-            })
-      }))
+      return Array.apply(null, Array(n)).map(function (cell) {
+        return makeCell(cell, rank, file++);
+      });
+    }));
+  });
+
+  if (b === Meteor.userId()) {
+    data.reverse();
+    data = data.map(function (row) {
+      return row.reverse();
     });
+  }
 
-    if (b === Meteor.userId()){
-      data.reverse();
-
-      data = data.map(function(row) {
-        return row.reverse();
-      })
-    }
-    return data
+  return data;
 }
 
 function makeCell(val, rank, file) {
@@ -130,33 +126,35 @@ function makeCell(val, rank, file) {
     piece: val,
     img: pieces[val] || '',
     cell: String.fromCharCode(97 + file) + rank
-  }
-};
+  };
+}
 
 function getGame() {
   return Games.findOne(FlowRouter.getParam('id'));
-};
+}
 
 function getMoves() {
   var chess = new Chess();
   chess.load_pgn(getGame().moves);
   var moves = chess.history();
 
-  if (Session.get('stepping')){
-    if (moves.length < Session.get('moveIndex')){
+  if (Session.get('stepping')) {
+
+    if (moves.length < Session.get('moveIndex')) {
       Session.set('moveIndex', moves.length);
     }
-    moves = moves.slice(0, Session.get('moveIndex'))
+
+    moves = moves.slice(0, Session.get('moveIndex'));
   }
-  return moves
-};
+
+  return moves;
+}
 
 Template.stepper.helpers({
   canStep: function (result) {
-    return result && !Session.get('stepping')
+    return result && !Session.get('stepping');
   },
-
-  stepping: function (result){
+  stepping: function () {
     return Session.get('stepping');
   }
 });
@@ -168,9 +166,9 @@ Template.stepper.events({
   },
   'click #prev': function (evt) {
     var idx = Session.get('moveIndex');
-    Session.set('moveIndex', idx <= 0 ? 0 : idx -1);
+    Session.set('moveIndex', idx <= 0 ? 0 : idx - 1);
   },
   'click #next': function (evt) {
     Session.set('moveIndex', Session.get('moveIndex') + 1);
   }
-})
+});
